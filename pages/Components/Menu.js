d11d601f89efb861ddc06f76f5263da89adf6898
Jeremy Shen <jeremyshen1987@gmail.com>
2023-05-08 17:18:41 -0600

    use Img to replace legacy img; disable lazy loading on logo img

--- pages/Components/Menu.js
+++ pages/Components/Menu.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link"
-import { useRouter } from "next/router";
 import Active_Link from "./Active_Link";
 import { useMainContext } from "@/utils/context";
 
