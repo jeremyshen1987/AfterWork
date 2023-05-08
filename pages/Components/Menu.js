cac9d38c32d2ab084731aa63750adf5687894b39
Jeremy Shen <jeremyshen1987@gmail.com>
2023-05-08 17:17:18 -0600

    use Img to replace legacy img

--- pages/Components/Menu.js
+++ pages/Components/Menu.js
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import Image from "next/image";
 import Link from "next/link"
 import { useRouter } from "next/router";
 import Active_Link from "./Active_Link";
@@ -16,18 +16,15 @@ export default function Menu(){
 
             <div className="container_logo">
                 <h4>Powered by:</h4>
-                <Link href='https://open.spotify.com/'><img src="/spotify_logo.svg" alt="sportify logo" className="menu_svg"></img></Link>
+                <Link href='https://open.spotify.com/'><Image src="/spotify_logo.svg" width={180} height={120} alt="sportify logo" priority='true' className="menu_svg" /></Link>
             </div>
 
 
-                {/* <Link href='/'>Home</Link> */}
                 <Active_Link href='/'>Home</Active_Link>
                 <Active_Link href='/search'>Search</Active_Link>
-                {/* <Link href='/search'>Search</Link> */}
                 <li className="relative">
                     <Active_Link href='/liked'>Liked</Active_Link>
 
-                    {/* <Link href='/liked'>Liked</Link> */}
                     {num > 0 && <span className="num_like">{num}</span>}
                 </li>
         </div>
