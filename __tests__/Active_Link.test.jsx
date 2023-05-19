abcaa2078c6b29f4e02dced73a1e30b114e0accb
Jeremy Shen <jeremyshen1987@gmail.com>
2023-05-19 16:04:29 -0600

    testing for ActiveLinks component

--- __tests__/Active_Link.test.jsx
+++ __tests__/Active_Link.test.jsx
@@ -1,34 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import { useRouter } from 'next/router';
-import Active_Link from '@/pages/Components/Active_Link';
 import '@testing-library/jest-dom';
+
+import Active_Link from '@/pages/Components/Active_Link';
+
+
+jest.mock('next/router', () => ({
+    useRouter: jest.fn()
+}))
+
+useRouter.mockReturnValue({
+    query: {},
+    asPath: '/',
+    push: () => {}
+})
+
  
 describe('Active_Link', () => {
 
-
-    jest.mock('next/router', () => ({
-        useRouter: jest.fn()
-    }))
-
-
-    it('renders a link', () => {
-
-        useRouter.mockReturnValue({ query: {}})
+    it('renders link and text', () => {
 
         render(
             <Active_Link href='/search'>
-                test page
+                Search
             </Active_Link>
         );
     
+        const router = useRouter()
 
         screen.debug()
-        // const heading = screen.getByRole('heading', {
-        //   name: /welcome to next\.js!/i,
-        // });
-    
-        // expect(heading).toBeInTheDocument();
-
+        
+        const anchor = screen.getByRole('link', {name: 'Search'})
+        expect(anchor).toBeInTheDocument();
+        expect(anchor).toHaveAttribute('href', '/search')
 
     });
 });
