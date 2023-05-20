ee05dfb66042a425c75590af5702320135d78de8
Jeremy Shen <jeremyshen1987@gmail.com>
2023-05-20 16:57:26 -0600

    unit test - Menu component

---
+++ __tests__/Menu.test.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom'
+import { useRouter } from 'next/router';
+import Menu from '@/pages/Components/Menu';
+import { MainProvider } from '@/utils/context';
+
+
+// jest.spyOn(useMainContext, 'likes').mockReturnValue([{id:1, type: 'test'}])
+// jest.replaceProperty(useMainContext, 'likes', [{id:1, type: 'test'}])
+
+// useMainContext.likes = jest.fn().mockReturnValue([{id:1, type: 'test'}])
+
+// jest.mock('@/utils/context', ()=>({
+//     useMainContext: jest.fn(()=>{
+//         return{
+//             likes: [{id:1, type: 'test'}],
+//             setLikes: {}
+//         }
+//     })
+// }))
+
+jest.mock('next/router', () => ({
+    useRouter: jest.fn()
+}))
+
+useRouter.mockReturnValue({
+    query: {},
+    asPath: '/',
+    push: () => {}
+})
+
+
+
+describe('Menu sidebar', ()=>{
+
+    beforeEach(()=>{
+        render (
+            <MainProvider >
+                <Menu />
+            </MainProvider>
+        )
+    })
+
+    it('have 3 tabs', ()=>{
+
+
+        expect(screen.getByRole('link', {name: 'Home'})).toBeInTheDocument()
+        expect(screen.getByRole('link', {name: 'Search'})).toBeInTheDocument()
+        expect(screen.getByRole('link', {name: 'Liked'})).toBeInTheDocument()
+
+        screen.debug()
+
+    })
+
+    it('have spotify logo, number of likes - 0 is not visible', ()=>{
+
+        expect(screen.getByRole('img')).toBeInTheDocument()
+        expect(screen.queryByText('0')).not.toBeInTheDocument()
+
+    })
+
+})
